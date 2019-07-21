f8965466f81cdf6bcc3c013507e1c4f017b95db5
Avi Lior <avi@lior.org>
2019-07-20 21:19:16 -0400

    Fixed method names

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ def get_hello():
     return {"Hello":"World V2"}
 
 @app.get("/api/healthy/", status_code=HTTP_200_OK)
-def get_health():
+def get_healthy():
     return "healthy"
 
 @app.get("/api/ready/", status_code=HTTP_200_OK)
-def get_health():
+def get_ready():
     return "ready"
 
 
