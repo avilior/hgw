b59c8438d331dff38256196723286977bb55b815
Avi Lior <avi@lior.org>
2019-07-24 01:08:22 -0400

    greeting via environment variable

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from starlette.status import *
 import logging
+import os
 
 LOG = logging.getLogger(__name__ )
 app = FastAPI()
 
 @app.get("/", status_code=HTTP_200_OK)
-def get_hello():
-    return {"Hello":"World V2"}
+def get_greeting():
+    greeting = os.getenv('GREETING',"Hello World - 2020")
+    return greeting
 
 @app.get("/api/healthz/", status_code=HTTP_200_OK)
 def get_healthz():
