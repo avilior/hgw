c9a379c8e7f272d9e10d9684558f31f338652031
Avi Lior <avi@lior.org>
2019-07-24 00:00:51 -0400

    added logging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from starlette.status import *
+import logging
 
+LOG = logging.getLogger(__name__ )
 app = FastAPI()
 
 @app.get("/", status_code=HTTP_200_OK)
@@ -9,10 +11,12 @@ def get_hello():
 
 @app.get("/api/healthz/", status_code=HTTP_200_OK)
 def get_healthz():
+    LOG.info("healthz called")
     return "healthy"
 
 @app.get("/api/readiness/", status_code=HTTP_200_OK)
 def get_readiness():
+    LOG.info("readiness called")
     return "ready"
 
 
